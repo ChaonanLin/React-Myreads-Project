1bae2b0400e1e4c4f4d941081d0501c5f9e3734d
Savoir <savoir.cn@gmail.com>
2018-12-04 23:23:40 -0800

    Extracting Book Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import './App.css'
+import { PropTypes }  from 'prop-types'
 
 class SearchBook extends React.Component {
     render() {
@@ -185,6 +186,12 @@ class BooksStatus extends React.Component {
                             <div className="book-authors">Mark Twain</div>
                           </div>
                         </li>
+                        <Book
+                            title= {'The Adventures of Tom Sawyer'}
+                            author={'Mark Twain'}
+                            imageURL={"http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api"}
+
+                        />
                       </ol>
                     </div>
                   </div>
@@ -198,13 +205,54 @@ class BooksStatus extends React.Component {
     }
 }
 
+class Book extends React.Component {
+    changeShelf = (e) => {
+        this.props.onShelfChange(e.targe.value)
+    }
+
+    render() {
+        return(
+            <li>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover" style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: `url("${this.props.imageURL}")`
+                     }}></div>
+                    <div className="book-shelf-changer">
+                      <select onChange={this.changeShelf} value={this.props.shelf}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="book-title">{this.props.title}</div>
+                  <div className="book-authors">{this.props.author}</div>
+                </div>
+            </li>
+        )
+    }
+
+    static propTypes = {
+    imageURL: PropTypes.object.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.array,
+    shelf: PropTypes.string.isRequired,
+    onShelfChange: PropTypes.func.isRequired
+  }
+}
+
+
 class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
         <Route path='/search' component={SearchBook}
         />
-
         <Route exact path='/' component={BooksStatus}
         />
       </div>
